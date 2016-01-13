34c4a7a61e47b713e7dd7a026e276a3d26700d30
Sean Zarringhalam <seanzarrin@gmail.com>
2016-01-13 00:59:06 -0800

    Added tests for npm-helper

--- helpers/npm-helper.js
+++ helpers/npm-helper.js
@@ -1,40 +1,53 @@
 'use strict';
 
-var sys = require('sys');
 var spawn = require('child_process').spawn;
-var path = require('path');
 var q = require('q');
 
-function repoSyntax(url) {
-	var regex = new RegExp('github.com/([^/]*/[^/]*)\.git');
-	var matches = url.match(regex);
-	return matches[1];
+function _addRepository(repos, module) {
+	var repoName;
+
+	if (module.repository && module.repository.url) {
+		repoName = getRepoName(module.repository.url);
+		if (repoName) {
+			repos[repoName] = true;
+		}
+	}
 }
 
+function getRepoName(url) {
+	var regex = new RegExp('github.com/([^/]*/[^/]*)\.git');
+	var matches = url.match(regex);
+	return matches && matches[1];
+}
 
-function getRepos () {
+function getRepos() {
 	var deferred = q.defer();
 	var proc = spawn('npm', ['ls', '--json', '--long']);
 	var stdout = '';
 
-	proc.stdout.on("data", function(chunk) {
+	proc.stdout.on("error", function (err) {
+		deferred.reject(err);
+	});
+
+	proc.stdout.on("data", function (chunk) {
 		stdout += chunk;
 	});
 
 	proc.stdout.on("end", function() {
-		var module = JSON.parse(stdout);
+		var module = JSON.parse(stdout || '{}');
 
 		// Mapping of module names to bug urls
-		var moduleBugs = {};
+		var repos = {};
 
 		// Include the current module in collection of issues
-		if (module.name && module.repository && module.repository.url) {
-			moduleBugs[repoSyntax(module.repository.url)] = true;
-		}
+		_addRepository(repos, module);
 
 		var queue = [];
 		var dependencies = module.dependencies;
-		queue.push(dependencies);
+
+		if (dependencies) {
+			queue.push(dependencies);
+		}
 
 		while (queue.length) {
 			dependencies = queue.pop();
@@ -46,18 +59,17 @@ function getRepos () {
 					queue.push(module.dependencies);
 				}
 
-				if (module.name && module.repository && module.repository.url) {
-					moduleBugs[repoSyntax(module.repository.url)] = true;
-				}
+				_addRepository(repos, module);
 			});
 		}
 
-		deferred.resolve(Object.keys(moduleBugs));
+		deferred.resolve(Object.keys(repos));
 	});
 
 	return deferred.promise;
 }
 
 module.exports = {
+	getRepoName: getRepoName,
 	getRepos: getRepos
 };
---
+++ test/helpers/npm-helper-test.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var sinon = require('sinon');
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var rewire = require('rewire');
+var npmHelper = rewire('../../helpers/npm-helper');
+
+var assert = chai.assert;
+
+sinon.assert.expose(assert, {prefix: ''});
+chai.use(chaiAsPromised);
+
+describe('npm-helper', function () {
+	var spawn, procOn;
+
+	beforeEach(function () {
+		procOn = sinon.stub();
+
+		spawn = sinon.stub().returns({
+			stdout: {
+				on: procOn
+			}
+		});
+
+		npmHelper.__set__('spawn', spawn);
+	});
+
+	describe('#getRepos', function () {
+
+		it('calls spawn with the proper arguments to npm', function () {
+			procOn.withArgs('end').yields();
+
+			return npmHelper.getRepos()
+				.then(function () {
+					return assert.calledWith(spawn, 'npm', ['ls', '--json', '--long']);
+				});
+		});
+
+		it('rejects when spawn passes an error', function () {
+			var error = new Error('sample error');
+
+			procOn.withArgs('error').yields(error);
+
+			var promise = npmHelper.getRepos();
+
+			assert.isRejected(promise, /sample error/);
+		});
+
+		it('resolves with names of repos for dependencies', function () {
+			var npmInfo = {
+				repository: {
+					url: 'git://github.com/owner1/repo1.git'
+				},
+				dependencies: {
+					package2: {
+						repository: {
+							url: 'git://github.com/owner2/repo2.git'
+						}
+					},
+					package3: {
+						repository: {
+							url: 'git://github.com/owner3/repo3.git'
+						},
+						dependencies: {
+							package4: {
+								repository: {
+									url: 'git://github.com/owner4/repo4.git4'
+								},
+								dependencies: {}
+							}
+						}
+					}
+				}
+			};
+
+			procOn.withArgs('data').yields(JSON.stringify(npmInfo));
+			procOn.withArgs('end').yields();
+
+			var promise = npmHelper.getRepos();
+
+			return promise.then(function (repoNames) {
+				assert.sameMembers(repoNames, ['owner1/repo1', 'owner2/repo2', 'owner3/repo3', 'owner4/repo4']);
+			});
+		});
+	});
+
+	describe('#getRepoName', function () {
+		it('retrieves the repo name from a github url', function () {
+			var url = 'git://github.com/owner/repo.git';
+
+			var repoName = npmHelper.getRepoName(url);
+
+			assert.equal(repoName, 'owner/repo');
+		});
+
+		it('returns undefined if it is unable to parse a repo name', function () {
+			var url = 'theresnoreponamehere';
+
+			var repoName = npmHelper.getRepoName(url);
+
+			assert.isNull(repoName);
+		});
+	});
+});
