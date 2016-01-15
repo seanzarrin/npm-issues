3a7e39e631691388cc83b4d9374c6c65e8d8f5d3
Sean Zarringhalam <seanzarrin@gmail.com>
2016-01-15 00:59:15 -0800

    Added nolimit option and set default limit to 10

--- bin/cli.js
+++ bin/cli.js
@@ -4,12 +4,16 @@
 var minimist = require('minimist');
 var npmDoctor = require('../lib/npm-doctor');
 var issueLogger = require('../lib/issue-logger');
+var chalk = require('chalk');
 
-var options = minimist(process.argv.slice(2));
+var options = minimist(process.argv.slice(2), {
+    boolean: 'no-limit'
+});
 
 var query = options._[0];
 var depth = options.depth;
 var limit = options.limit;
+var noLimit = options['nolimit'];
 
 if (!query) {
     console.log([
@@ -17,15 +21,23 @@ if (!query) {
         'Usage: npm-doctor [options] [query]',
         '',
         'Examples: npm-doctor "An issue that I need to search for"',
+        '          npm-doctor --nolimit "An issue that I need to search for"',
         '          npm-doctor --depth 2 "An issue that I need to search for"',
         '          npm-doctor --depth 2 --limit 10 "An issue that I need to search for"',
         '',
         'Options:',
         '--depth [int]    The maximum depth of your local node modules that should be included in the search',
-        '--limit [int]    The maximum number of results you would like logged to console'
+        '--limit [int]    (defaults to 10) The maximum number of results you would like logged to console',
+        '--nolimit       Removes the default limit of 10 issues for logging'
     ].join('\n'));
 
     return;
+}
+
+// Default limit is 10
+if (!noLimit && !limit) {
+    console.log(chalk.green('Using default limit of 10. To increase this, choose a limit or specify nolimit'));
+    limit = 10;
 }
 
 npmDoctor.searchIssues(query, depth)
